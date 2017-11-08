36ecb26cbc700b030b54b56d5cd4c9d54fecba86
Vojko Rajh <vojko.rajh@gmail.com>
2017-11-08 15:51:07 +0100

    add navbar and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+
+  # static pages
+    get "/static_pages/:page" => "static_pages#show"
+    # get 'static_pages/o_meni'
+    # get 'static_pages/maria_montessori'
+  # end static pages
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
